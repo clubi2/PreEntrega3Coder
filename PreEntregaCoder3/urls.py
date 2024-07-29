8cdb025da0fa7ba3ea5c2e339347279ea3566114
Valentin Bustamante <vbustamante@INFOANDINA.LOCAL>
2024-07-29 13:13:40 -0300

    Paths+views

--- PreEntregaCoder3/urls.py
+++ PreEntregaCoder3/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import saludo
+from .views import saludo, dia_de_hoy, muestra_nombre, probando_template
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
+    path('diaDeHoy/', dia_de_hoy),
+    path('muestra_nombre/<nombre>/', muestra_nombre),
+    path('probando_template/', probando_template),
 ]
--- PreEntregaCoder3/views.py
+++ PreEntregaCoder3/views.py
@@ -1,4 +1,33 @@
 from django.http import HttpResponse
+from datetime import datetime
+from django.template import Template, Context
 
 def saludo(request):
     return HttpResponse("Hola mundo!, hola Coder!")
+
+def dia_de_hoy(request):
+    dia = datetime.now()
+    return HttpResponse(f"Hoy es día:<br>{dia}")
+
+def muestra_nombre(request, nombre):
+    return HttpResponse(f"Buenos días {nombre}, bienvenido a Coder")
+
+def probando_template(request):
+
+    # Abrimos el archivo html
+    mi_html = open('./PreEntregaCoder3/plantillas/index.html')
+
+    # Creamos el template haciendo uso de la clase Template
+    plantilla = Template(mi_html.read())
+
+    # Cerramos el archivo previamente abierto, ya que lo tenemos cargado en la variable plantilla
+    mi_html.close()
+
+    # Creamos un contexto, más adelante vamos a aprender a usarlo, ahora lo necesitamos aunque sea vacío para que funcione
+    mi_contexto = Context()
+
+    # Terminamos de construír el template renderizándolo con su contexto
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
+
